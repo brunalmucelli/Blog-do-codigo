8963a40f4b2f61855b67a941d8be10e209186140
brunalmucelli <brunalmucelli@gmail.com>
2023-05-26 16:24:25 -0300

    Construção do login

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const {estrategiasAutenticacao} = require('./src/usuarios')
+
+
+
 app.use(
   bodyParser.urlencoded({
     extended: true
